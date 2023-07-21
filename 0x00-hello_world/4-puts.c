a10d9e7fbd49859efe8b62181a1d40adf7905144
mohamed1615 <mohamedhany1615@gmail.com>
2023-07-21 01:19:59 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include "stdio.h"
-
+/**
+ * main - Prints "Hello" followed by a new line
+ *
+ * Return: Always 0 (Success)
+ *    
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
