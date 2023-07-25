2f028df40aadf69c62015c8b8a06db86c6a745bf
mohamed1615 <mohamedhany1615@gmail.com>
2023-07-25 00:13:36 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,7 @@
 int main(void)
 {
 	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t length = sizeof(message) -1;
 	write(STDERR_FILENO, message, sizeof(message)-1);
 	return (1);
 }
