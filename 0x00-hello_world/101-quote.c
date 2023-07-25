2e0ce8be069a92046838933f68f563209ca58b3b
mohamed1615 <mohamedhany1615@gmail.com>
2023-07-25 00:09:43 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,15 @@
-#!/bin/bash
-gcc -Wall -o output -xc - <<EOF && ./output >&2 || echo "Compilation Failed"
-#include <unistd.h>
-int main(void) { write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); return 1; }
-EOF
+#include "unistd.h"
+
+/**
+ * main - do something
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	const char* msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, message, sizeof(message)-1);
+	return (1);
+}
