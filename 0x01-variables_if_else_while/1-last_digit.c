377775e32ad2166a79e5af28904a5c1b175ba628
mohamedhany1615 <mohamedhany1615@gmail.com>
2023-09-10 03:19:08 -0700

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	{
 		printf("%d is greater than 5\n", n%10);
 	}
-	else if (n%10 < 6 && != 0)
+	else if (n%10 < 6 && n%10 != 0)
 	{
 		printf("%d and is than 6 and not 0\n", n%10);
 	}
