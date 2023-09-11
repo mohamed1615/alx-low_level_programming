6603a9f733a7cb1c8ead297bab15058da0747b36
mohamedhany1615 <mohamedhany1615@gmail.com>
2023-09-11 02:47:48 -0700

    update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: Always 0 (Success)
+ */
+in main(void)
+{
+	int tens = 0;
+	int ones;
+
+	while (tens < 9)
+	{
+		ones = tens + 1;
+		while(ones <=9)
+		{
+			putchar(tens + '0');
+			putchar(ones + '0');
+			if (tens != 8 || ones != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			ones ++;
+		}
+		tens++;
+	}
+	putchar('\n');
+	return (0);
+}
