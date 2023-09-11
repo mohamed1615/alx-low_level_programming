65f0efbd38230a2650cc41a57235590471920050
mohamedhany1615 <mohamedhany1615@gmail.com>
2023-09-11 01:58:44 -0700

    update

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,12 +8,13 @@
 */
 int main(void)
 {
-    int i = 0;
-    
-    while (i < 10)
-    {
-        printf("%d", i);
-        i++;
-    }
-    printf("\n");
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
 }
