243a28399e9aea4cc9c5b1f9ed6bf4038fcb5b1c
mohamed1615 <57901178+mohamed1615@users.noreply.github.com>
2023-09-11 13:06:12 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -26,23 +26,6 @@ int main(void)
 			}
 		}
 	}
-	for (i = 0; i <= 6; i++)
-	{
-		for (j = i + 1; j <= 8; j++)
-		{
-			for (k = j + 1; k <= 9; k++)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(k + '0');
-				if (i != 6 || j != 7 || k != 8)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
 	putchar('\n');
 	return (0);
 }
