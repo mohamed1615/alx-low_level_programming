d0d5a83dfe12a017e0ac43fb8c72d57172490510
mohamedhany1615 <mohamedhany1615@gmail.com>
2023-09-11 02:26:56 -0700

    update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,20 @@ int main(void)
 {
 	int n = 0;
 
-	while (n < 9)
+	while (n < 10)
 	{
 		putchar(n + '0');
-		putchar(',');
-		putchar(' ');
-		n++;
+		if (n == '9')
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+			n++;
+		}
 	}
-	putchar('9');
+	putchar('\n');
 	return (0);
 }
