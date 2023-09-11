7e7e3c04918083c9458f78bf65f1ca7c749ac608
mohamedhany1615 <mohamedhany1615@gmail.com>
2023-09-11 03:04:06 -0700

    update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,13 +2,14 @@
 
 /**
  * main - prints digits combinations
- * Return : Always 0 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i <= 7; i++)
+	for (i = 0; i <= 6; i++)
 	{
 		for (k = j + 1; j <= 8; j++)
 		{
@@ -17,7 +18,24 @@ int main(void)
 				putchar(i + '0');
 				putchar(j + '0');
 				putchar(k + '0');
-				if (i != 7 || j != 8 || k != 9)
+				if (i != 6 || j != 7 || k != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	for (i = 0; i <= 6; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i != 6 || j != 7 || k != 8)
 				{
 					putchar(',');
 					putchar(' ');
